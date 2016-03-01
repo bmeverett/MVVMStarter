c11a75286dcbe2843c9ef072c8383d872bc911d5
Brandon Everett <bme2010@gmail.com>
2016-03-01 15:21:01 -0500

    initial commit

---
+++ MVVM Starter/DelegateCommand.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace MVVM_Starter
+{
+   public class DelegateCommand : ICommand
+    {
+
+        private readonly Predicate<object> _canExecute;
+        private readonly Action<object> _execute;
+
+        private readonly Action _exec;
+
+        #region Construction
+
+        public DelegateCommand(Action<object> execute)
+            : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+
+        public DelegateCommand(Action execute) : this(execute, null)
+        {
+
+        }
+        public DelegateCommand(Action execute, Predicate<object> CanExecute)
+        {
+            _exec = execute;
+            _canExecute = CanExecute;
+
+        }
+        #endregion
+
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+       public void Execute()
+        {
+            if (_exec != null)
+                _exec.Invoke();
+            else
+                _execute(null);
+        }
+
+        public void Execute(object parameter)
+        {
+            if(parameter == null && _exec != null)
+            {
+                _exec.Invoke();
+                return;
+            }
+            _execute(parameter);
+        }
+
+        public void RaiseExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+    }
+}
---
+++ MVVM Starter/ViewModels/BaseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace MVVM_Starter.ViewModels
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
